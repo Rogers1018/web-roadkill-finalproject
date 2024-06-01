a35b83627c96062eb4c8d8da40a59eae33e4da47
陳政宏 <110602689+Rogers1018@users.noreply.github.com>
2024-06-01 18:54:44 +0800

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
